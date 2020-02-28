0c86772d1da53206e93857d547b361ffbe9b68f5
srinishasiri <srinisha2628@gmail.com>
2020-02-28 12:36:39 +0530

    prgrm

---
+++ calc.py
@@ -0,0 +1,20 @@
+a = int(input("enter value\n"))
+b = int(input("enter value\n"))
+def add(a,b):
+    print("addition of two numbers is",a+b)
+
+def sub(a,b):
+    print("substarction os two numbers is",a-b)
+
+def mul(a,b):
+    print("multiplication of two numbers is",a*b)
+
+def div(a,b):
+    if(b==0):
+        print("b value cannot be zero")
+    else:
+        print("division of two numbers is",a/b)
+add(a,b)
+sub(a,b)
+mul(a,b)
+div(a,b)
