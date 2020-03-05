d77391949d3a8258fb91f79cc73bae485704a2f6
srinishasiri <srinisha2628@gmail.com>
2020-03-05 07:30:08 +0530

    calc_prgrm

--- calc.py
+++ calc.py
@@ -1,22 +1,11 @@
-import sys
-a = int(sys.argv[1])
-b = int(sys.argv[2])
-def add(a,b):
-    print("addition of two numbers is",a+b)
+def add(a, b):
+    return a+b
 
-def sub(a,b):
-    print("substarction os two numbers is",a-b)
+def sub(a, b):
+    return a-b
 
-def mul(a,b):
-    print("multiplication of two numbers is",a*b)
+def mul(a, b):
+    return a*b
 
-def div(a,b):
-    if(b==0):
-        print("b value cannot be zero")
-    else:
-        print("division of two numbers is",a/b)
-print("succesfull")
-add(a,b)
-sub(a,b)
-mul(a,b)
-div(a,b)
+#def div(a, b):
+ #   return a/b
